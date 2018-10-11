7e69a9099be9f7022c616abeecafb0d443560434
moli <1020245514@qq.com>
2018-10-11 21:12:59 +0800

    初始化study项目

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("爸爸终于配置好了！");
+        System.out.println("终于配置好了！");
+        System.out.println("开始学习数据结构和算法了");
     }
 }
