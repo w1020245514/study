c20badacd9a3637191f506cfefacd4622a1073a1
w1020245514 <1020245514@qq.com>
2018-10-12 16:44:10 +0800

    idea中提交

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!sdfsdf");
+        System.out.println("Hello World!sdf");
 
     }
 }
