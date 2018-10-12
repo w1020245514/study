0698932a3bd2ee1ed8942dd7724649821c8f0069
w1020245514 <1020245514@qq.com>
2018-10-12 16:38:12 +0800

    idea中提交

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("终于配置好了！");
-        System.out.println("开始学习数据结构和算法了");
+        System.out.println("Hello World!sdf");
+
     }
 }
